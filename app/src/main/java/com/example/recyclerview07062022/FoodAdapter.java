944f6d78ce9be1d813b06318694bc11fafe42ad3
Phat <phatdroid94@gmail.com>
2022-07-21 20:46:35 +0700

    update id

---
+++ app/src/main/java/com/example/recyclerview07062022/FoodAdapter.java
@@ -0,0 +1,19 @@
+package com.example.recyclerview07062022;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * Created by pphat on 7/21/2022.
+ */
+public class FoodAdapter{
+
+    class FoodViewHolder extends RecyclerView.ViewHolder {
+
+        public FoodViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
